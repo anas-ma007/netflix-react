b109ca5a55e513c1b1d05ace9a0157943c7ff00f
anas-ma007 <anaschelakkara786@gmail.com>
2023-10-24 23:36:38 +0530

    make responsive

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,35 +1,41 @@
-import React, { useEffect } from 'react'
-import './Banner.css'
-import axios from '../../axios'
-import {API_KEY, imageUrl} from '../../constants/constants';
-import { useState } from 'react';
+import React, { useEffect } from "react";
+import "./Banner.css";
+import axios from "../../axios";
+import { API_KEY, imageUrl } from "../../constants/constants";
+import { useState } from "react";
 
 function Banner() {
-    const [movie, setMovie] = useState()
-    useEffect(()=>{
-        axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
-            console.log(response.data.results[0], "banner console");
-            setMovie(response.data.results[0])
-        })
-    }, [])
+  const [movie, setMovie] = useState();
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`
+      )
+      .then((response) => {
+        console.log(response.data.results[0], "banner console");
+        setMovie(response.data.results[0]);
+      });
+  }, []);
 
-    return (
-        <div style={{backgroundImage:`url(${movie ? imageUrl+movie.backdrop_path: '' })`}} className='banner'>
-            <div className='content'>
-                <h1 className='title'>{movie?movie.title:""}</h1>
-                <div className='banner_buttons'>
-                    <button className='button'> Play</button>
-                    <button className='button'> My List</button>
-                </div>
-                <h1 className='description'> {movie?movie.overview:""} </h1>
-            </div>
-
-            <div className="fade_bottom"></div>
-
-
-
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${movie ? imageUrl + movie.backdrop_path : ""})`,
+      }}
+      className="banner"
+    >
+      <div className="content">
+        <h1 className="title">{movie ? movie.title : ""}</h1>
+        <div className="banner_buttons">
+          <button className="button"> Play</button>
+          <button className="button"> My List</button>
         </div>
-    )
+        <h1 className="description"> {movie ? movie.overview : ""} </h1>
+      </div>
+
+      <div className="fade_bottom"></div>
+    </div>
+  );
 }
 
-export default Banner
+export default Banner;
--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -1,59 +1,66 @@
-import { useEffect, useState } from 'react';
-import React from 'react'
-import './RowPost.css'
-import axios from '../../axios'
-import { imageUrl,API_KEY } from '../../constants/constants'
-import Youtube from 'react-youtube'
+import { useEffect, useState } from "react";
+import React from "react";
+import "./RowPost.css";
+import axios from "../../axios";
+import { imageUrl, API_KEY } from "../../constants/constants";
+import Youtube from "react-youtube";
 
 function RowPost(props) {
-    const [movies, setMovies] = useState([])
-    const [urlId, setUrlId]= useState("")
-    useEffect(() => {
-        axios.get(props.url).then(response => {
-            console.log(response.data, "row console");
-            setMovies(response.data.results)
-        }).catch(err => {
-            alert('network Error')
-        })
-    }, [])
-    const handleMovie= (id)=>{
-        console.log(id, "movie id");
-        axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(response=>{
-            console.log(response.data,"youtube videos array");
-            if(response.data.results.length!==0){
-                setUrlId(response.data.results[0])
+  const [movies, setMovies] = useState([]);
+  const [urlId, setUrlId] = useState("");
+  useEffect(() => {
+    axios
+      .get(props.url)
+      .then((response) => {
+        console.log(response.data, "row console");
+        setMovies(response.data.results);
+      })
+      .catch((err) => {
+        alert("network Error");
+      });
+  }, []);
+  const handleMovie = (id) => {
+    console.log(id, "movie id");
+    axios
+      .get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`)
+      .then((response) => {
+        console.log(response, "response in link");
+        console.log(response.data, "youtube videos array");
+        if (response.data.results.length !== 0) {
+          setUrlId(response.data.results[0]);
+        } else {
+          console.log("Videos not available");
+        }
+      })
+      .catch((err) => {
+        alert(`no videos available for this id ${id}`);
+      });
+  };
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+  return (
+    <div className="row">
+      <h2>{props.title}</h2>
+      <div className="posters">
+        {movies.map((obj) => (
+          <img
+            onClick={() => handleMovie(obj.id)}
+            className={props.isSmall ? "smallPoster" : "poster"}
+            alt="poster"
+            src={`${imageUrl + obj.backdrop_path}`}
+          />
+        ))}
+      </div>
 
-            } else {
-                console.log('Videos not available');
-            }
-        
-        })
-
-
-    }
-    const opts = {
-        height: '390',
-        width: '100%',
-        playerVars: {
-          // https://developers.google.com/youtube/player_parameters
-          autoplay: 1,
-        },
-      };
-    return (
-        <div className='row'>
-            <h2>{props.title}</h2>
-            <div className='posters'>
-                {movies.map((obj) =>
-                    <img onClick={()=>handleMovie(obj.id)} className={props.isSmall ? 'smallPoster' : 'poster'} alt="poster" src={`${imageUrl + obj.backdrop_path}`} />
-                )}
-            </div>
-
-           { urlId && <Youtube opts={opts} videoId={urlId.key} />}
-
-
-
-        </div>
-    )
+      {urlId && <Youtube opts={opts} videoId={urlId.key} />}
+    </div>
+  );
 }
 
-export default RowPost
+export default RowPost;
